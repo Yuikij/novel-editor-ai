88dd6dc0a5c9e7ca938ff72dd4b8e2437e981981
zxj <775718540@qq.com>
2025-04-27 17:41:19 +0800

    style(novel-main): 优化 WebMvcConfig 类格式
    - 删除多余的空行
    - 添加必要的空行

--- novel-main/src/main/java/com/soukon/novelEditorAi/config/WebMvcConfig.java
+++ novel-main/src/main/java/com/soukon/novelEditorAi/config/WebMvcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.util.pattern.PathPatternParser;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    
+
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         PathPatternParser pathPatternParser = new PathPatternParser();
